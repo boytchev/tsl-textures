425ac8efb395d08fa82e268f6f6f408a5510d956
boytchev <70705048+boytchev@users.noreply.github.com>
2024-08-04 15:53:59 +0300

    Update protozoa.js

--- src/protozoa.js
+++ src/protozoa.js
@@ -9,39 +9,46 @@ import { noise } from 'tsl-textures/tsl-utils.js';
 
 
 
+var pnoise = tslFn( ([ pos, fat ])=>{
+
+	return noise( pos ).mul( fat ).clamp( -3.14, 3.14 ).cos().add( 1 ).div( 2 );
+
+} );
+
+
+
 var protozoa = tslFn( ( params )=>{
 
+	var pos = positionLocal.mul( exp( params.scale.sub( 1 ) ) ).add( params.seed ).toVar( );
 
-	var scale = params.scale.toVar();
-	var pos = positionLocal.mul( exp( scale.sub( 1 ) ) ).add( params.seed ).toVar( );
+	var matcap = vec3( matcapUV, matcapUV.length() ).toVar();
 
-	var mc = vec3( matcapUV, matcapUV.length() ).toVar();
+	var rings1 = float( 0 ).toVar();
+	var rings2 = float( 0 ).toVar();
 
+	var n1 = float( 0 ).toVar();
+	var n2 = float( 0 ).toVar();
 
-	var n = float( 0 ).toVar();
-	var nc = float( 0 ).toVar();
-	var factor = float( 1 ).toVar();
 	var fat = params.fat.add( 0.2 ).oneMinus().mul( 60 ).add( 30 ).toVar();
-	var scl = float( 2 ).toVar();
+	var scale = float( 2 ).toVar();
 
 	var dPos = params.amount.div( 2 ).add( 0.5 ).exp().toVar();
 
 	loop( 10, ()=>{
 
-		var rings1 = noise( pos.add( mc ) ).mul( fat ).clamp( -3.14, 3.14 ).cos().add( 1 ).div( 2 ).toVar();
-		var rings2 = noise( pos.yzx.add( mc ) ).mul( fat ).clamp( -3.14, 3.14 ).cos().add( 1 ).div( 2 ).toVar();
+		rings1.assign( pnoise( pos.xyz.add( matcap ), fat ) );
+		rings2.assign( pnoise( pos.yzx.add( matcap ), fat ) );
 
-		n.addAssign( rings1.mul( rings2 ).mul( scl ) );
-		nc.addAssign( rings1.max( rings2 ).mul( scl ) );
+		n1.addAssign( rings1.mul( rings2 ).mul( scale ) );
+		n2.addAssign( rings1.max( rings2 ).mul( scale ) );
 
-		pos.mulAssign( dPos );
-		pos.assign( mix( pos, vec3( 0, 0, 0 ), 0.4 ) );
-		factor.addAssign( 10 );
-		scl.mulAssign( 0.9 );
+		pos.assign( mix( pos.mul( dPos ), 0, 0.4 ) );
+
+		scale.mulAssign( 0.9 );
 
 	} );
 
-	return mix( params.background, mix( params.color, params.subcolor, nc.mul( 0.1 ) ), n );
+	return mix( params.background, mix( params.color, params.subcolor, n2.mul( 0.1 ) ), n1 );
 
 } );
 
