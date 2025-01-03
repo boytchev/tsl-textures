9608d8ea16bf81281110d0892131471b3959fce4
boytchev <70705048+boytchev@users.noreply.github.com>
2025-01-03 12:24:09 +0200

    Update online.js

--- online/online.js
+++ online/online.js
@@ -5,7 +5,7 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as lil from "three/addons/libs/lil-gui.module.min.js";
 import { mergeVertices } from 'three/addons/utils/BufferGeometryUtils.js';
-import { dynamic, overlayPlanar, showFallbackWarning } from 'tsl-textures/tsl-utils.js';
+import { dynamic, hideFallbackWarningm, overlayPlanar, showFallbackWarning } from 'tsl-textures/tsl-utils.js';
 
 const THREEJS = '0.171.0';
 const TSLTEXTURES = '1.5.0';
@@ -94,6 +94,8 @@ onResize( );
 
 function animationLoop( /*t*/ ) {
 
+	hideFallbackWarning( );
+	
 	if ( mixer ) {
 
 		mixer.update( 1/144 );
