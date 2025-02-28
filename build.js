8274def185be420d62f3569752c338769f3817ac
Mark Roland <radiowhir@gmail.com>
2025-02-28 10:36:17 -0800

    Removed NPM support test code

--- build.js
+++
@@ -1,9 +0,0 @@
-const esbuild = require('esbuild');
-
-esbuild.build({
-  entryPoints: ['npm-test/main.js'],
-  bundle: true,
-  minify: true,
-  sourcemap: true,
-  outfile: 'npm-test/dist/bundle.js',
-}).catch(() => process.exit(1));
