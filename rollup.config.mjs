3da1c174dacb63103f58e639e64c4658ce52ca6c
Mark Roland <radiowhir@gmail.com>
2025-04-04 15:36:50 -0700

    Added version to dist files
    Replaced outdated rollup-plugin-uglify with @rollup/plugin-terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,26 +1,28 @@
-import { uglify } from 'rollup-plugin-uglify';
+import terser from '@rollup/plugin-terser';
+import pkg from './package.json' with { type: 'json' };
 
 export default [
-
 	{
 		input: './src/tsl-textures.js',
 		output: {
 			file: './dist/tsl-textures.js',
 			format: 'es',
-			banner: '// TSL Textures\n',
+			banner: `// TSL Textures v${pkg.version}\n`,
 		},
 	},
-
 	{
 		input: './src/tsl-textures.js',
 		output: {
 			file: './dist/tsl-textures.min.js',
 			format: 'es',
-			banner: '// TSL Textures\n',
+			banner: `/*! TSL Textures v${pkg.version} */\n`,
 		},
 		plugins: [
-			uglify()
+			terser({
+				format: {
+					comments: 'some'
+				}
+			})
 		]
-	},
-
-]
+	}
+];
