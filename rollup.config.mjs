fa8bf2fb65e01db57f5200cf586584eae5f598da
Mark Roland <radiowhir@gmail.com>
2025-04-04 15:43:04 -0700

    Added CommonJS module support

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,8 @@
+import commonjs from '@rollup/plugin-commonjs';
+import resolve from '@rollup/plugin-node-resolve';
 import terser from '@rollup/plugin-terser';
+import fs from 'fs';
+import path from 'path';
 import pkg from './package.json' with { type: 'json' };
 
 export default [
@@ -18,6 +22,39 @@ export default [
 			banner: `/*! TSL Textures v${pkg.version} */\n`,
 		},
 		plugins: [
+			terser({
+				format: {
+					comments: 'some'
+				}
+			})
+		]
+	},
+	{
+		input: './src/tsl-textures.js',
+		output: {
+			file: `dist/cjs/tsl-textures.js`,
+			format: 'cjs',
+			banner: `// TSL Textures v${pkg.version}\n`,
+			sourcemap: false
+		},
+		external: ['three', 'three/tsl'],
+		plugins: [
+			resolve(),
+			commonjs()
+		]
+	},
+	{
+		input: './src/tsl-textures.js',
+		output: {
+			file: `dist/cjs/tsl-textures.min.js`,
+			format: 'cjs',
+			banner: `/*! TSL Textures v${pkg.version} */\n`,
+			sourcemap: false
+		},
+		external: ['three', 'three/tsl'],
+		plugins: [
+			resolve(),
+			commonjs(),
 			terser({
 				format: {
 					comments: 'some'
